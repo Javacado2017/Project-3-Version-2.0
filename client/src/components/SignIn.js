5fda8685dd32726412ac8822da99ba8481fe4aba
unknown <newtonsapple2017@gmail.com>
2018-07-31 16:22:42 -0400

    client side componenets

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-// import { Link } from 'react-router';
+
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
--- client/src/sub-components/NewsArticleWidget.js
+++ client/src/sub-components/NewsArticleWidget.js
@@ -15,50 +15,37 @@ const styles = {
 function SimpleMediaCard(props) {
   const { classes } = props;
   return (
-    <div class='container'>
-    <div class='col s2'>
-    </div>
-
-    <div class='col s8'>
-      <Card style={styles}>
-      <div class='row'>
-        <div class='col s6'>
-        <CardMedia>
-          <img
-            height='250'
-            width="100%"
-            src="https://www.ccn.com/wp-content/uploads/2017/06/Facebook-notifications.jpg"
-            alt=""
-          />
-        </CardMedia>
+    <Card style={styles}>
+      <div class="row">
+        <div class="col s6">
+          <CardMedia>
+            <img
+              width="100%"
+              src="https://www.ccn.com/wp-content/uploads/2017/06/Facebook-notifications.jpg"
+              alt=""
+            />
+          </CardMedia>
         </div>
-        <div class='col s6'>
-        <CardContent>
-          <Typography gutterBottom variant="headline" component="h2">
-            Opinion | How Blockchain Could Fix Facebook's Fake News Problem
-          </Typography>
-          <Typography component="p">
-            Facebook has a big fake news problem, and the company can solve that
-            issue by taking a serious look at blockchain technology.
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small" color="primary">
-            <a href="https://www.ccn.com/opinion-how-blockchain-could-fix-facebooks-fake-news-problem/">
-              Learn More
-            </a>
-          </Button>
-        </CardActions>
+        <div class="col s6">
+          <CardContent>
+            <Typography gutterBottom variant="headline" component="h2">
+              Opinion | How Blockchain Could Fix Facebook's Fake News Problem
+            </Typography>
+            <Typography component="p">
+              Facebook has a big fake news problem, and the company can solve
+              that issue by taking a serious look at blockchain technology.
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small" color="primary">
+              <a href="https://www.ccn.com/opinion-how-blockchain-could-fix-facebooks-fake-news-problem/">
+                Learn More
+              </a>
+            </Button>
+          </CardActions>
         </div>
       </div>
-      </Card>
-    </div>
-
-    <div class='col s2'>
-    </div>
-    </div>
-
-
+    </Card>
   );
 }
 
